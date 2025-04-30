3368e413e58c256f4274976c180917b6c2394d03
S.Péter <164756229+ezP0tat0@users.noreply.github.com>
2025-04-30 12:30:39 +0200

    Update Class.cs remove price

--- GymWebapp/GymWebapp/Model/Data/Class.cs
+++ GymWebapp/GymWebapp/Model/Data/Class.cs
@@ -9,7 +9,6 @@ namespace GymWebapp.Model.Data
         public string Description { get; set; }
         public DateTime Date { get; set; }
         public int Duration { get; set; }
-        public int Price { get; set; }
 
         [ForeignKey("Trainer")]
         public int TrainerId { get; set; }
